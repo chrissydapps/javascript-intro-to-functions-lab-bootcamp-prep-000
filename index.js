1963925b407563d674c53d92a0c4a17009ac0fa6
cdapaaaddo@gmail.com <cdapaaaddo@gmail.com>
2018-06-06 19:17:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,13 +15,13 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (logWhisper(string) === string) {
+  if (string.toLowerCase === string) {
     return "I can't hear you!"
   }
 }
 
 function sayHiToGrandma(string) {
-  if (logShout(string) === string) {
+  if (string.toUpperCase === string) {
     return "YES INDEED!"
   }
 }
