b8d268b3b67fcea389d90b155e6de5cf11e6a767
cdapaaaddo@gmail.com <cdapaaaddo@gmail.com>
2018-06-06 18:48:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function sayHiToGrandma(string) {
   if (string === whisper(string)) {
   return `"I can't hear you!"`
 
-function sayHiToGrandma(string) {  
+function sayHiToGrandma(string) {
 } if (string === string.toUpperCase()) {
   return `"YES INDEED!"`
 
