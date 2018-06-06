7a9b221d39099b1a3b7b4533d263dab607af1556
cdapaaaddo@gmail.com <cdapaaaddo@gmail.com>
2018-06-06 19:06:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function sayHiToGrandma(string) {
 }
 
 function sayHiToGrandma(string) {
- if (string === shout(string)) {
+ if (string === string.toUpperCase()) {
    return "YES INDEED!"
   }
 }
